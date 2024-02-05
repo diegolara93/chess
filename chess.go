f54aac7e2aa2fe15817356078b37d43362779942
diegolara <diegolara93345@gmail.com>
2024-02-04 22:43:08 -0600

    Added all pieces

--- chess.go
+++ chess.go
@@ -104,6 +104,18 @@ func (c *ChessBoard) printCurrentBoard() {
 }
 
 func main() {
+	whiteKing := King{1, 5, "white"}
+	blackKing := King{8, 5, "black"}
+	whiteQueen := Queen{1, 4, "white"}
+	blackQueen := Queen{8, 4, "black"}
+	whiteKnight1 := Knight{1, 2, "white"}
+	whiteKnight2 := Knight{1, 7, "white"}
+	blackKnight1 := Knight{8, 2, "black"}
+	blackKnight2 := Knight{8, 7, "black"}
+	whiteBishop1 := Bishop{1, 3, "white"}
+	whiteBishop2 := Bishop{1, 6, "white"}
+	blackBishop1 := Bishop{8, 3, "black"}
+	blackBishop2 := Bishop{8, 6, "black"}
 	whiteLeftRook := Rook{1, 1, "white"}
 	whiteRightRook := Rook{1, 8, "white"}
 	blackLeftRook := Rook{8, 1, "black"}
@@ -125,7 +137,11 @@ func main() {
 	blackPawn7 := Pawn{7, 7, "black"}
 	blackPawn8 := Pawn{7, 8, "black"}
 
-	chessBoard := ChessBoard{[]ChessPiece{&whiteLeftRook, &whiteRightRook, &blackLeftRook, &blackRightRook,
+	chessBoard := ChessBoard{[]ChessPiece{
+		&whiteLeftRook, &whiteRightRook, &blackLeftRook, &blackRightRook,
+		&whiteKnight1, &whiteKnight2, &blackKnight1, &blackKnight2,
+		&whiteBishop1, &whiteBishop2, &blackBishop1, &blackBishop2,
+		&whiteKing, &blackKing, &whiteQueen, &blackQueen,
 		&whitePawn1, &whitePawn2, &whitePawn3, &whitePawn4, &whitePawn5, &whitePawn6, &whitePawn7, &whitePawn8,
 		&blackPawn1, &blackPawn2, &blackPawn3, &blackPawn4, &blackPawn5, &blackPawn6, &blackPawn7, &blackPawn8}}
 	chessBoard.printCurrentBoard()
